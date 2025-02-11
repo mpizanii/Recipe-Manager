9086b4f539757822b5d31f0a0a6f58b2eb31cf4e
mpizanii <mpizani28@gmail.com>
2025-02-10 21:38:17 -0300

    começando as views

--- usuario/views.py
+++ usuario/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+
+        usuario = authenticate(request, username = email, password = senha)
+
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('home')
+        else:
+            messages.error(request, "Usuário ou senha incorreto. Tente novamente.")
+            return redirect('usuario:login')
+        
+    return render(request, 'login.html', {"messages":messages})
 
