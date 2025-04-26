dbd7da74548e3baf8307caf24a93765cce0a3ebf
Matheus Pizani <mpizani28@gmail.com>
2025-04-25 23:20:15 -0300

    feat: deploy do projeto com banco de dados

--- recipe_manager/settings.py
+++ recipe_manager/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -53,24 +54,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'recipe_manager.wsgi.application'
 
-if os.getenv("RAILWAY_ENVIRONMENT"):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv("PGDATABASE"),
-            'USER': os.getenv("PGUSER"),
-            'PASSWORD': os.getenv("PGPASSWORD"),
-            'HOST': os.getenv("PGHOST"),
-            'PORT': os.getenv("PGPORT"),
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+        ssl_require=False
+    )
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
